33411081f6bdccda5ad68b5fa88d2abea488424e
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-04-07 17:12:06 +0700

    asd

--- lessonmanagement/admin.py
+++ lessonmanagement/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
 admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(GroupSubject)
+admin.site.register(SubjectManager)
+admin.site.register(GroupSubjectManager)
+admin.site.register(ClassYear)
+admin.site.register(ClassYearManager)
+
+
+
--- lessonmanagement/models.py
+++ lessonmanagement/models.py
@@ -17,13 +17,103 @@ class Teacher(models.Model):
     def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
         return 'user_{0}/{1}'.format(instance.user.id, filename)
-    avatar = models.ImageField("Ảnh đại diện", help_text = "Ảnh nên được crop về ảnh vuông để đạt được độ thẩm mỹ cao nhất",upload_to = "test/", blank = True, null = True)
+
+    # Bổ sung trình crop ảnh vào tempate
+    avatar = models.ImageField("Ảnh đại diện", help_text = "Ảnh nên được crop về ảnh vuông để đạt được độ thẩm mỹ cao nhất",upload_to = user_directory_path, blank = True, null = True)
 
 
     def __str__(self):
         return '%s %s' % (self.firstname, self.lastname)
 
 
+class SubjectAbstract(models.Model):
+    title = models.CharField(max_length=20, verbose_name="Tên môn học", help_text="Tên chính xác của môn học theo quy định của Bộ Giáo Dục")
+    description = models.TextField("Mô tả môn học", blank=True)
+    coverimage = models.ImageField("Ảnh bìa cho môn học", upload_to = "subject/", null = True, blank = True)
+    
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Subject(SubjectAbstract):
+    group = models.ForeignKey("GroupSubject", on_delete=models.SET_NULL, null=True, blank=True)
+
+class GroupSubject(SubjectAbstract):
+    pass
+
+
+class ManagerAbstract(models.Model):
+    manager = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
+    startdate = models.DateField(blank=True, null=True)
+    enddate = models.DateField(blank=True, null=True)
+    note = models.CharField("Ghi chú", max_length=200,blank=True, null=True)
+
+
+    class Meta:
+        abstract = True
+
+
+
+class SubjectManager(ManagerAbstract):
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return '%s %s %s' % (self.manager.firstname, self.manager.lastname, self.subject.title)
+
+class GroupSubjectManager(ManagerAbstract):
+    group = models.ForeignKey(GroupSubject, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return '%s %s %s' % (self.manager.firstname, self.manager.lastname, self.group.title)
+
+
+class ClassYear(models.Model):
+    TITLE_CHOICES = [
+    ('a', 'A'),
+    ('b', 'B'),
+    ('c', 'C'),
+    ('d', 'D')
+    ]
+    title = models.CharField(verbose_name="Tên lớp",max_length=1, choices=TITLE_CHOICES)
+    startyear = models.IntegerField()
+
+    def __str__(self):
+        return '%s %s' % (self.title, self.startyear)
+
+    def class_status(self):
+        now = datetime.now()
+        if (self.startyear + 3) > now.year:
+            return "Đang đào tạo"
+        else:
+            return "Đã tốt nghiệp"
+
+    def class_level(self):
+        now = datetime.now()
+        if (self.startyear) == now.year:
+            return "6"
+        elif (self.startyear + 1) == now.year:
+            return "7"
+        elif (self.startyear + 2) == now.year:
+            return "8"
+        elif (self.startyear + 3) == now.year:
+            return "9"
+        else:
+            return "Đã tốt nghiệp"
+
+class ClassYearManager(ManagerAbstract):
+    class_year = models.ForeignKey(ClassYear, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    upload_time = models.DateTimeField(auto_now=True)
+
+    def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+        return 'lesson/user_{0}/{1}'.format(instance.user.id, filename)
+    lesson_path = models.FileField(upload_to=user_directory_path)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 
