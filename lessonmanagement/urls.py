2a3b1cbdd5672343860f0a828be99476cd6fba22
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-04-09 16:53:18 +0700

    sdfsdfs

--- lessonmanagement/urls.py
+++ lessonmanagement/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login")
+
     
 ] 
--- lessonmanagement/views.py
+++ lessonmanagement/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
 
 def index(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'base.html', context)
+    else:
+        return redirect(login)
+
+def login(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pwd = request.POST.get('password')
+        user = authenticate(username=username, password=pwd)
+
+        if user is not None:
+            login(request, user)
+        else:
+            print("fuck wrong")
     context = {}
-    return render(request, 'base.html', context)
+    return render(request, 'login.html', context)
+
+
+
+    
