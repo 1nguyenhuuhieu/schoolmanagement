75786b971446869e4bd01068e5749eec138cbf2d
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-04-11 13:07:03 +0700

    a

--- lessonmanagement/views.py
+++ lessonmanagement/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.models import User
 
 
 def index(request):
-    context = {}
+    lesson = Lesson.objects.filter(teacher=request.user.teacher.id)
+    context = {'lesson_list': lesson}
     return render(request, 'base.html', context)
 
 
