80de5aec02308352c1096339b305f0d661ce3d3d
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-04-15 13:26:51 +0700

    1

--- lessonmanagement/admin.py
+++ lessonmanagement/admin.py
@@ -4,12 +4,10 @@ from .models import *
 # Register your models here.
 class SubjectClassYearInline(admin.TabularInline):
     model = SubjectClassYear
-
 class ClassYearManagerInline(admin.TabularInline):
     model = ClassYearManager
-
-
-
+class SubjectTeacherInline(admin.TabularInline):
+    model = SubjectTeacher
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     # date_hierarchy = 'birth_date'
@@ -18,16 +16,10 @@ class TeacherAdmin(admin.ModelAdmin):
     def fullname(self, obj):
         return '%s %s' % (obj.firstname, obj.lastname)
     list_filter  = ('main_subject','is_work')
-    inlines = [SubjectClassYearInline, ClassYearManagerInline]
-    
-
+    inlines = [SubjectTeacherInline, SubjectClassYearInline, ClassYearManagerInline]
 @admin.register(ClassYear)
 class ClassYearAdmin(admin.ModelAdmin):
-
-
-
     list_display = ['class_title_year', 'startyear','class_year_manager_name']
-
     @admin.display()
     def class_year_manager_name(self, obj):
         class_year_manager = ClassYearManager.objects.filter(class_year_id = obj.id)
@@ -40,31 +32,25 @@ class ClassYearAdmin(admin.ModelAdmin):
                     return i.teacher
         else:
             return "Chưa có Giáo viên chủ nhiệm"
-
-
-    
-
     list_filter  = ('title',)
-
-    
-
     inlines = [SubjectClassYearInline, ClassYearManagerInline]
-
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     pass
 
 
 
-# admin.site.register(Subject)
 admin.site.register(GroupSubject)
-# admin.site.register(SubjectTeacher)
-# admin.site.register(GroupSubjectManager)
+
+@admin.register(SubjectTeacher)
+class SubjectTeacher(admin.ModelAdmin):
+    pass
+admin.site.register(GroupSubjectManager)
 
 admin.site.register(ClassYearManager)
 admin.site.register(Lesson)
-# admin.site.register(SubjectClassYear)
-# admin.site.register(LessonClassYear)
+admin.site.register(SubjectClassYear)
+admin.site.register(LessonClassYear)
 
 
 
