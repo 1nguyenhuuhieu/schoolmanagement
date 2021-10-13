358dee0fde3d346b895657ca1a1f96b91ed4210b
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-10-13 14:43:22 +0700

    sdfsdf

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -41,15 +41,6 @@ INSTALLED_APPS = [
     'lessonmanagement',
     'django.contrib.humanize',
     'import_export',
-    'dbbackup',
-    'django_crontab',
-]
-
-DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
-DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'backup'}
-
-CRONJOBS = [
-    ('*/1 * * * *', 'schoolmanagement.cron.my_backup')
 ]
 
 MIDDLEWARE = [
