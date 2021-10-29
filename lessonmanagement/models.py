aafdea19261a111dc0e6ec9650c146b3b9a61a18
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-10-29 10:30:34 +0700

    Update models.py

--- lessonmanagement/models.py
+++ lessonmanagement/models.py
@@ -108,6 +108,7 @@ def now_week_schoolyear(schoolyear):
 # đổi ngày bất kì sang tuần tương ứng của năm học
 def day_week_schoolyear(schoolyear, d):
     start_day = schoolyear.start_date
+    print(start_day)
     start_monday = start_day + datetime.timedelta(days=-start_day.weekday())
     week = (d - start_monday)/7
     print(d)
